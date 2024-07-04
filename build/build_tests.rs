26c334fc6aa283d0df3ed873f410aa5a5898b977
HoneyPony <honeyponygames@gmail.com>
2024-07-03 20:31:45 -0400

    Add print_inner_return.poni (test)

--- build/build_tests.rs
+++ build/build_tests.rs
@@ -40,6 +40,7 @@ pub fn generate(tests_file: &mut File) {
 		("print/", "print_void_first"             , Expected::Output("oh no my pony\n")),
 		("print/", "print_void_others"            , Expected::Output("oh no my pony\n")),
 		("print/", "print_dif_funs"               , Expected::Output("debug: 123\ndebug behavior: calling helper: 10\n")),
+		("print/", "print_inner_return"           , Expected::Output("afterwards\n")),
 
 		("globals/", "global_block" , Expected::Output("7\n")),
 		// TODO: Figure out precise float output format we want.
