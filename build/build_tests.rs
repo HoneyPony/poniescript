d3a9d8ea3a467fcacb53a5b4461823e922658788
HoneyPony <honeyponygames@gmail.com>
2024-07-04 13:13:44 -0400

    Implement 1 test in the new system

--- build/build_tests.rs
+++ build/build_tests.rs
@@ -1,29 +1,14 @@
 use std::fs::File;
 use std::io::Write as _;
 
-enum Expected {
-	Output(&'static str),
-	Error
-}
-
-impl std::fmt::Display for Expected {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match self {
-			Expected::Output(output) => write!(f, "Expected::Output(\"{output}\")"),
-			Expected::Error => write!(f, "Expected::Error"),
-		}
-	}
-}
-
-fn generate_test(file: &mut File, path: &str, test_name: &str, expect: Expected) -> std::io::Result<()> {
+fn generate_test(file: &mut File, path: &str, test_name: &str) -> std::io::Result<()> {
 	writeln!(file, "#[test]")?;
 	writeln!(file, "fn {test_name}() {{")?;
 
 	// We let the generated test code actually do the concat!.
-	let c_path = format!("concat!(env!(\"CARGO_TARGET_TMPDIR\"), \"/{test_name}.c\")");
 	let exe_path = format!("concat!(env!(\"CARGO_TARGET_TMPDIR\"), \"/{test_name}\")");
 
-	writeln!(file, "\trun_integration_test(\"tests/poni/{path}{test_name}.poni\", {c_path}, {exe_path}, {expect});")?;
+	writeln!(file, "\trun_integration_test(\"tests/poni/{path}{test_name}.poni\", {exe_path});")?;
 	writeln!(file, "}}")?;
 
 	Ok(())
@@ -32,56 +17,56 @@ fn generate_test(file: &mut File, path: &str, test_name: &str, expect: Expected)
 pub fn generate(tests_file: &mut File) {
 	let tests = [
 		// print_nested: the last line is 345 because it should re-print each of the inner print()s.
-		("print/", "print_nested"                 , Expected::Output("345\n45\n5\n345\n")),
-		("print/", "print_string_literal"         , Expected::Output("hello world\n")),
-		("print/", "print_multiple_string_literal", Expected::Output("hello world\n")),
-		("print/", "print_number_literal"         , Expected::Output("3\n")),
-		("print/", "print_block"                  , Expected::Output("hello pony folk\n")),
-		("print/", "print_void_first"             , Expected::Output("oh no my pony\n")),
-		("print/", "print_void_others"            , Expected::Output("oh no my pony\n")),
-		("print/", "print_dif_funs"               , Expected::Output("debug: 123\ndebug behavior: calling helper: 10\n")),
-		("print/", "print_inner_return"           , Expected::Output("afterwards\n")),
+		//("print/", "print_nested"                 , Expected::Output("345\n45\n5\n345\n")),
+		("print/", "print_string_literal"),
+		//("print/", "print_multiple_string_literal", Expected::Output("hello world\n")),
+		//("print/", "print_number_literal"         , Expected::Output("3\n")),
+		//("print/", "print_block"                  , Expected::Output("hello pony folk\n")),
+		//("print/", "print_void_first"             , Expected::Output("oh no my pony\n")),
+		//("print/", "print_void_others"            , Expected::Output("oh no my pony\n")),
+		//("print/", "print_dif_funs"               , Expected::Output("debug: 123\ndebug behavior: calling helper: 10\n")),
+		//("print/", "print_inner_return"           , Expected::Output("afterwards\n")),
 
-		("globals/", "global_block" , Expected::Output("7\n")),
+		//("globals/", "global_block" , Expected::Output("7\n")),
 		// TODO: Figure out precise float output format we want.
-		("globals/", "globals_3"    , Expected::Output("3\n4.000000\n10\n")),
-		("globals/", "global_global", Expected::Output("55\n")),
+		//("globals/", "globals_3"    , Expected::Output("3\n4.000000\n10\n")),
+		//("globals/", "global_global", Expected::Output("55\n")),
 
-		("typecheck/", "print_assume_int", Expected::Output("10\n")),
-		("typecheck/", "promote_assumes_inside_block", Expected::Output("15\n")),
-		("typecheck/", "promote_to_float_arithmetic", Expected::Output("30.000000\n")),
-		("typecheck/", "promote_to_float_assign", Expected::Output("10.000000\n")),
-		("typecheck/", "str_types", Expected::Output("hello my little ponies\n")),
-		("typecheck/", "return_block_return", Expected::Output("")), // Make sure this one at least compiles
+		//("typecheck/", "print_assume_int", Expected::Output("10\n")),
+		//("typecheck/", "promote_assumes_inside_block", Expected::Output("15\n")),
+		//("typecheck/", "promote_to_float_arithmetic", Expected::Output("30.000000\n")),
+		//("typecheck/", "promote_to_float_assign", Expected::Output("10.000000\n")),
+		//("typecheck/", "str_types", Expected::Output("hello my little ponies\n")),
+		//("typecheck/", "return_block_return", Expected::Output("")), // Make sure this one at least compiles
 		// TODO: Add other tests when we get function calls
 	
-		("string/", "simple_str", Expected::Output("hello world25\n")),
-		("string/", "str_of_strbuf", Expected::Output("hello my little ponies\n")),
+		//("string/", "simple_str", Expected::Output("hello world25\n")),
+		//("string/", "str_of_strbuf", Expected::Output("hello my little ponies\n")),
 	
-		("functions/", "parse_params", Expected::Output("")),
-		("functions/", "param_trailing_comma", Expected::Output("")),
-		("functions/", "call_basic", Expected::Output("3\n7\n11\n")),
-		("functions/", "call_nested", Expected::Output("my little pony!\n")),
-		("functions/", "call_promote", Expected::Output("1.000000\n2.000000\n3.000000\n")),
-		("functions/", "void_fun", Expected::Output("ponies\nhorses\nponies\nhorses\n")),
+		//("functions/", "parse_params", Expected::Output("")),
+		//("functions/", "param_trailing_comma", Expected::Output("")),
+		//("functions/", "call_basic", Expected::Output("3\n7\n11\n")),
+		//("functions/", "call_nested", Expected::Output("my little pony!\n")),
+		//("functions/", "call_promote", Expected::Output("1.000000\n2.000000\n3.000000\n")),
+		//("functions/", "void_fun", Expected::Output("ponies\nhorses\nponies\nhorses\n")),
 		// Note: Test inspired by Crafting Interpreters 
-		("functions/", "fib", Expected::Output("9227465\n")),
+		//("functions/", "fib", Expected::Output("9227465\n")),
 
-		("scope/", "block_shadow", Expected::Output("hello ponies\nhello again ponies\nhello ponies\n")),
+		//("scope/", "block_shadow", Expected::Output("hello ponies\nhello again ponies\nhello ponies\n")),
 
-		("if/", "basic_if_expr_ret"   , Expected::Output("10\n3\n")),
-		("if/", "basic_if_expr_var"   , Expected::Output("3\n10\n")),
-		("if/", "basic_if_expr"       , Expected::Output("10\n3\n")),
-		("if/", "basic_if"            , Expected::Output("true\n")),
-		("if/", "if_no_else"          , Expected::Output("little pony\nmy\nlittle pony\n")),
+		//("if/", "basic_if_expr_ret"   , Expected::Output("10\n3\n")),
+		//("if/", "basic_if_expr_var"   , Expected::Output("3\n10\n")),
+		//("if/", "basic_if_expr"       , Expected::Output("10\n3\n")),
+		//("if/", "basic_if"            , Expected::Output("true\n")),
+		//("if/", "if_no_else"          , Expected::Output("little pony\nmy\nlittle pony\n")),
 		// Shows that the plain 'if' never returns a value.
-		("if/", "if_no_else_in_print" , Expected::Output("my little pony\n")),
+		//("if/", "if_no_else_in_print" , Expected::Output("my little pony\n")),
 
-		("comparison/", "compare_basic", Expected::Output("false\ntrue\ntrue\ntrue\n")),
-		("comparison/", "compare_constants", Expected::Output("true\ntrue\nfalse\nfalse\nfalse\ntrue\nfalse\ntrue\ntrue\ntrue\n"))
+		//("comparison/", "compare_basic", Expected::Output("false\ntrue\ntrue\ntrue\n")),
+		//("comparison/", "compare_constants", Expected::Output("true\ntrue\nfalse\nfalse\nfalse\ntrue\nfalse\ntrue\ntrue\ntrue\n"))
 	];
 
-	for (path, test, expect) in tests {
-		generate_test(tests_file, path, test, expect).unwrap();
+	for (path, test) in tests {
+		generate_test(tests_file, path, test).unwrap();
 	}
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,13 +4,7 @@ use std::{io::Read, path::PathBuf, process::{Command, Stdio}};
 
 const CC: Option<&str> = option_env!("PONI_CC");
 
-enum Expected {
-	Output(&'static str),
-	Error
-}
-
-fn test_should_print(input_file: &str, c_path: &str, exe_path: &str, output: &str) -> std::io::Result<()> {
-	let _ = std::fs::remove_file(c_path);
+fn test_should_print(input_file: &str, exe_path: &str) -> std::io::Result<()> {
 	let _ = std::fs::remove_file(exe_path);
 
 	// Additionally remove the exe path extended with ".exe", as on Windows
@@ -20,59 +14,21 @@ fn test_should_print(input_file: &str, c_path: &str, exe_path: &str, output: &st
 	
 	let mut poniescript = Command::new(env!("CARGO_BIN_EXE_poniescript"))
 		.arg("-o")
-		.arg(c_path)
+		.arg(exe_path)
 		.arg(input_file)
 		.arg("--no-timing")
+		.arg("--test")
 		.spawn()?;
 	let code = poniescript.wait()?;
-	if !code.success() {
-		panic!("poniescript should be able to compile this");
-	}
 
-	let mut cc = Command::new(CC.unwrap_or("gcc"))
-		.arg("-std=c11")
-		.arg(c_path)
-		.arg("-o")
-		.arg(exe_path)
-		.arg("-I")
-		.arg(".")
-		.spawn()?;
-	let code = cc.wait()?;
-	if !code.success() {
-		panic!("the c compiler should be able to compile this");
-	}
-
-	// Finally, actually test the program.
-	let mut testprog = Command::new(exe_path)
-		.stdout(Stdio::piped())
-		.spawn()?;
-
-	let mut stdout = testprog.stdout.take().expect("stdout");
-	testprog.wait()?;
-
-	let mut got = String::new();
-	stdout.read_to_string(&mut got)?;
-
-	// Unfortunately, we have to strip '\r' from the output. This does mean we
-	// can't compare strings with '\r' in our tests, at least for now.
-	let mut got_real = String::new();
-	got_real.reserve(got.len());
-	for c in got.chars() {
-		if c != '\r' { got_real.push(c); }
-	}
-
-	assert_eq!(output, got_real);
+	assert!(code.success());
 
 	Ok(())
 }
 
-fn run_integration_test(input_file: &str, c_path: &str, exe_path: &str, expect: Expected) {
+fn run_integration_test(input_file: &str, exe_path: &str) {
 	// TODO:
 	// - Use CARGO_TARGET_TMPDIR to store compiled programs
 	// - Have the test programs report the expected value/error
-	match expect {
-		Expected::Output(output) => 
-			assert!(test_should_print(input_file, c_path, exe_path, output).is_ok()),
-		Expected::Error => todo!(),
-	}
+	assert!(test_should_print(input_file, exe_path).is_ok())
 }
