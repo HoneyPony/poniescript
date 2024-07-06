15c198145ca55625f1f86a4260c4ca49550f7531
HoneyPony <honeyponygames@gmail.com>
2024-07-06 19:44:51 -0400

    Add test call_bottom_notreal.poni
    Showing that even if no such function exists in the program, we can
    still assign a value of Bottom to it

--- build/build_tests.rs
+++ build/build_tests.rs
@@ -86,6 +86,7 @@ pub fn generate(tests_file: &mut File) {
 		("call/", "returns_fun_fun"),
 		("call/", "returns_fun_fun_weirder"),
 		("call/", "call_bottom"),
+		("call/", "call_bottom_notreal"),
 	];
 
 	for (path, test) in tests {
