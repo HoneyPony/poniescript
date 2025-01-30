a353a47d015c1e5f2d3b3803cfdee36383814e0f
HoneyPony <honeyponygames@gmail.com>
2025-01-30 16:54:32 -0500

    Add dead_args test

--- build/build_tests.rs
+++ build/build_tests.rs
@@ -122,6 +122,7 @@ pub fn generate(tests_file: &mut File) {
 		("dead_code/", "dead_block"),
 		("dead_code/", "dead_binary1"),
 		("dead_code/", "dead_ops"),
+		("dead_code/", "dead_args"),
 	];
 
 	for (path, test) in tests {
