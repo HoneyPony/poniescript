4b8d20e1a38b9ee4a18ae19a0e055dcab739143a
HoneyPony <honeyponygames@gmail.com>
2025-10-19 16:38:08 -0400

    Add import optional test case that explores promote semantics

--- poniescript/build/build_tests.rs
+++ poniescript/build/build_tests.rs
@@ -323,6 +323,7 @@ pub fn generate(tests_file: &mut File) {
 		("optional/", "horse_question_mark"),
 		("optional/", "implicit_some"),
 		("optional/", "nil"),
+		("optional/", "opt_else_promote"),
 		("optional/", "option_else"),
 		("optional/", "option_else_ret"),
 		("optional/", "option_else_ret_shadow"),
