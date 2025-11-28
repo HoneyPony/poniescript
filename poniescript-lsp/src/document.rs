e83656cb351a8fb11c68c5578b19866688bc3be2
HoneyPony <honeyponygames@gmail.com>
2025-11-28 15:28:31 -0500

    Disable LSP compilation feature for now

--- poniescript-lsp/src/document.rs
+++ poniescript-lsp/src/document.rs
@@ -115,35 +115,35 @@ impl SourceProvider for LSPSource {
     }
 }
 
-fn do_finish_compile(db: &mut Db, mut ast: Ast, output: &PathBuf, args: &Args) -> Ast {
-    poniescript_core::dead_code::eliminate_dead_code(db, &mut ast);
+// fn do_finish_compile(db: &mut Db, mut ast: Ast, output: &PathBuf, args: &Args) -> Ast {
+//     poniescript_core::dead_code::eliminate_dead_code(db, &mut ast);
 
-    // Sort value types.
-    if db.sort_value_types().is_err() {
-        // TODO: Report these errors
-        return ast;
-    }
+//     // Sort value types.
+//     if db.sort_value_types().is_err() {
+//         // TODO: Report these errors
+//         return ast;
+//     }
 
-    // Pass 5: Codegen
-    // Generate any caches that require type checking info.
-    db.generate_codegen_caches(&args);
+//     // Pass 5: Codegen
+//     // Generate any caches that require type checking info.
+//     db.generate_codegen_caches(&args);
 
-    // TODO: Figure out how to make this async correctly...?
-    let Ok(mut output) = std::fs::File::create(output) else {
-        eprintln!("Warning: Unable to create output C file");
-        return ast;
-    };
+//     // TODO: Figure out how to make this async correctly...?
+//     let Ok(mut output) = std::fs::File::create(output) else {
+//         eprintln!("Warning: Unable to create output C file");
+//         return ast;
+//     };
 
-    let (ast, sources) = ast.into_readonly();
-    let ast = Arc::new(ast);
+//     let (ast, sources) = ast.into_readonly();
+//     let ast = Arc::new(ast);
 
-    if let Err(err) = poniescript_core::codegen::codegen(&args, db, Arc::clone(&ast), &mut output) {
-        eprintln!("Warning: Unable to write C file: {err}");
-    }
+//     if let Err(err) = poniescript_core::codegen::codegen(&args, db, Arc::clone(&ast), &mut output) {
+//         eprintln!("Warning: Unable to write C file: {err}");
+//     }
 
-    let Ok(ast) = Arc::try_unwrap(ast) else { unreachable!() };
-    Ast::from_readonly(ast, sources)
-}
+//     let Ok(ast) = Arc::try_unwrap(ast) else { unreachable!() };
+//     Ast::from_readonly(ast, sources)
+// }
 
 fn do_handle_files(store: &DocumentStore) -> (Db, Ast, Diagnostics, HashMap<Url, SourceId>, HashMap<SourceId, Url>) {
     eprintln!("--- re-parse modules ---");
@@ -199,7 +199,7 @@ fn do_handle_files(store: &DocumentStore) -> (Db, Ast, Diagnostics, HashMap<Url,
         args.engine = true;
 
         if let Some(output) = &store.c_output {
-            ast = do_finish_compile(&mut db, ast, output, &args);
+            //ast = do_finish_compile(&mut db, ast, output, &args);
         }
     }
     
