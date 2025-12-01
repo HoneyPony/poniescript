f8aa8af3ba8ce3ee8f2cb5db78f208f25958eae3
HoneyPony <honeyponygames@gmail.com>
2025-12-01 18:09:23 -0500

    Initial barebones implementation of a build system
    Yippee...

---
+++ poni-build-cli/src/main.rs
@@ -0,0 +1,118 @@
+use std::{fs::{self, File}, path::Path, process::Command};
+
+use clap::{Parser, Subcommand};
+use poni_build::{BuildConfig, ConfigReadError, EnvironmentConfig, read_configs};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommand
+}
+
+#[derive(Subcommand)]
+enum CliCommand {
+    Build {
+        project: Option<String>,
+    },
+    Run {
+        project: String,
+    }
+}
+
+fn show_error_msg(msg: &str) -> ! {
+    eprintln!("error: {msg}");
+    std::process::exit(1);
+}
+
+fn do_build(build: &BuildConfig, env: &EnvironmentConfig, project: Option<&String>) {
+    // Check the project before doing anything else.
+    if let Some(project) = project {
+        if !build.projects.contains_key(project) {
+            eprintln!("error: no such project '{}'", project);
+            std::process::exit(1);
+        }
+    }
+
+    // First, create the .build folder and the build.ninja file.
+    fs::create_dir_all(".build")
+        .unwrap_or_else(|_| show_error_msg("couldn't create .build directory"));
+
+    // TODO: Don't rebuild .ninja file when nothing has changed... :/
+    let mut file = File::create(".build/build.ninja")
+        .unwrap_or_else(|_| show_error_msg("couldn't create .build/build.ninja file"));
+
+    build.generate_ninja_file(&mut file, env)
+        .unwrap_or_else(|_| show_error_msg("couldn't write .build/build.ninja file"));
+
+    // Now, spawn the ninja process.
+    // TODO: Configurable ninja path?
+    let mut process = Command::new("ninja");
+    process.arg("-f").arg(".build/build.ninja")
+        .env("NINJA_STATUS", "%e /) ");
+
+    // If we're only building one project, then pass that as an argument.
+    if let Some(project) = project {
+        process.arg(project);
+    }
+    
+    // Spawn the process.
+    let mut child = process.spawn()
+        .unwrap_or_else(|_| show_error_msg("couldn't spawn 'ninja' process"));
+
+    child.wait()
+        .unwrap_or_else(|_| show_error_msg("couldn't wait for 'ninja' process"));
+}
+
+fn show_error(err: ConfigReadError) -> ! {
+    match err {
+        ConfigReadError::NoPoniesToml => eprintln!("error: couldn't open 'ponies.toml'"),
+        ConfigReadError::BadPoniesToml(err) => eprintln!("error: couldn't parse 'ponies.toml':\n{err}"),
+
+        // TODO: These errors should include their path with them, if possible.
+        ConfigReadError::NoEnvironmentToml => eprintln!("error: couldn't open build-config.toml (in $XDG_CONFIG_HOME)"),
+        ConfigReadError::BadEnvironmentToml(err) => eprintln!("error: couldn't parse build-config.toml:\n{err}"),
+        ConfigReadError::XdgError => eprintln!("error: couldn't read XDG environment"),
+    }
+
+    std::process::exit(1);
+}
+
+fn read_files() -> (BuildConfig, EnvironmentConfig) {
+    // Read the build config from the local directory
+    let result = read_configs(Path::new("./"));
+
+    match result {
+        Ok(configs) => configs,
+        Err(err) => show_error(err)
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // Right now, all the commands require all the configuration files.
+    let (build, env) = read_files();
+
+    // Now do stuff, depending on what the command was.
+    match cli.command {
+        CliCommand::Build { project } => {
+            do_build(&build, &env, project.as_ref());
+        },
+        CliCommand::Run { project } => {
+            if !build.projects.contains_key(&project) {
+                eprintln!("error: no such project '{}'", project);
+                std::process::exit(1);
+            }
+            do_build(&build, &env, Some(&project));
+
+            // Now run that specific project.
+            // TODO: Support arguments to the project?
+            let mut child = Command::new(project)
+                .spawn()
+                .unwrap_or_else(|_| show_error_msg("couldn't spawn child process."));
+
+            child.wait()
+                .unwrap_or_else(|_| show_error_msg("couldn't wait for child process."));
+        },
+    }
+}
---
+++ poni-build/src/lib.rs
@@ -0,0 +1,109 @@
+use std::{collections::HashMap, fs::{self, File}, io::Write, path::{Path, PathBuf}};
+
+use microxdg::{XdgApp, XdgError};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct EnvironmentConfig {
+    pub poni_h_path: PathBuf,
+    pub poni_gc_path: PathBuf,
+    pub poniescript_path: PathBuf,
+
+    // TODO: We probably want these per-platform in some way.
+    pub cc: String,
+    pub linker: String,
+}
+
+/// The top-level configuration for the build, described by a .toml file,
+/// at least for now.
+#[derive(Serialize, Deserialize)]
+pub struct BuildConfig {
+    pub projects: HashMap<String, Project>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Project {
+    /// The .poni files that make up this project.
+    pub files: Vec<PathBuf>,
+
+    /// .h files that are imported for this project.
+    pub imports: Vec<PathBuf>,
+}
+
+impl BuildConfig {
+    pub fn generate_ninja_file(&self, file: &mut File, env: &EnvironmentConfig) -> std::io::Result<()> {
+        writeln!(file, "builddir .build\n")?;
+
+        // TODO: Make all the Paths Strings instead?
+        writeln!(file, "poni_h_path = {}", env.poni_h_path.display())?;
+        writeln!(file, "poni_gc_path = {}", env.poni_gc_path.display())?;
+        writeln!(file, "poniescript = {}\n", env.poniescript_path.display())?;
+
+        // TODO: What we will end up wanting is a bunch of different cc/linkers,
+        // used for debug/release, and used for different target platforms.
+        // (e.g. ideally you should be able to, from Linux, easily compile for
+        // Linux, Windows, Web, and Android)
+        writeln!(file, "rule link\n  command = {} $in -o $out -L$poni_gc_path -lponiescript_gc\n  description = link\n",
+            env.linker)?;
+        writeln!(file, "rule cc\n  command = {} -c $in -o $out -I$poni_h_path -I.\n  description = cc\n",
+            env.cc)?;
+
+        writeln!(file, "rule poni-debug\n  command = $poniescript -o $out $in $imports --no-timing\n  description = poniescript\n")?;
+        
+        for (name, project) in &self.projects {
+            writeln!(file, "build {}-debug: link .build/{}-debug.o", name, name)?;
+            writeln!(file, "build .build/{}-debug.o: cc .build/{}-debug.c", name, name)?;
+            write!(file, "build .build/{}-debug.c: poni-debug", name)?;
+            
+            // TODO: Escape spaces in paths
+            for poni in &project.files {
+                write!(file, " {}", poni.display())?;
+            }
+            write!(file, "\n  imports =")?;
+            for import in &project.imports {
+                write!(file, " -i {}", import.display())?;
+            }
+            write!(file, "\n\n")?;
+        }
+
+        write!(file, "default")?;
+        for (name, _) in &self.projects {
+            write!(file, " {name}")?;
+        }
+        write!(file, "\n")?;
+
+        Ok(())
+    }
+}
+
+pub enum ConfigReadError {
+    NoPoniesToml,
+    BadPoniesToml(String),
+    NoEnvironmentToml,
+    BadEnvironmentToml(String),
+    XdgError,
+}
+
+impl From<XdgError> for ConfigReadError {
+    fn from(_: XdgError) -> Self {
+        ConfigReadError::XdgError
+    }
+}
+
+pub fn read_configs(build_config_search_path: &Path) -> Result<(BuildConfig, EnvironmentConfig), ConfigReadError> {
+    let app = XdgApp::new("poniescript")?;
+
+    let config_str = fs::read_to_string(app.config_file("build-config.toml")?)
+        .map_err(|_| ConfigReadError::NoEnvironmentToml)?;
+
+    let env: EnvironmentConfig = toml::from_str(&config_str)
+        .map_err(|err| ConfigReadError::BadEnvironmentToml(err.to_string()))?;
+
+    let build_str = fs::read_to_string(build_config_search_path.join("ponies.toml"))
+        .map_err(|_| ConfigReadError::NoPoniesToml)?;
+
+    let build: BuildConfig = toml::from_str(&build_str)
+        .map_err(|err| ConfigReadError::BadPoniesToml(err.to_string()))?;
+
+    Ok((build, env))
+}
