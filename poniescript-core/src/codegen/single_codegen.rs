25ed12a4454e0e94308dcdcc75851c010ef59d84
HoneyPony <honeyponygames@gmail.com>
2025-12-08 12:37:42 -0500

    Add log::trace! for codegen issue

--- poniescript-core/src/codegen/single_codegen.rs
+++ poniescript-core/src/codegen/single_codegen.rs
@@ -1177,6 +1177,7 @@ impl<'a> Codegen<'a> {
 			// so return its value.
 			(last, val) => {
 				let last = self.compile_stmt(ast, *last.unwrap(), into);
+				log::trace!("codegen: line: {} is_some(): {}", block.location.offset, last.is_some());
 				let last = last.unwrap();
 				if last.needs_storage() && val.needs_storage() {
 					// Grab fresh copy of indent() because we're in the block,
