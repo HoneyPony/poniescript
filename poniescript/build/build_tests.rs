b134d9004ad7d25095275932c91c85bf140526dd
HoneyPony <honeyponygames@gmail.com>
2026-01-24 20:46:36 -0500

    Some @inner tests with two @inner classes

--- poniescript/build/build_tests.rs
+++ poniescript/build/build_tests.rs
@@ -319,6 +319,10 @@ pub fn generate(tests_file: &mut File) {
 		("classes/", "inner_dyn_funcall"),
 		("classes/", "inner_dyn_funcapture_getter"),
 		("classes/", "inner_dyn_funcall_getter"),
+		("classes/", "inner_dyn2_funcapture"),
+		("classes/", "inner_dyn2_funcall"),
+		("classes/", "inner_dyn2_funcapture_getter"),
+		("classes/", "inner_dyn2_funcall_getter"),
 
 		("classes/", "inner_err_not_dyn"),
 		("classes/", "inner_err_not_dyn_get"),
