83d26571fe185cf1090fe90b604302fd2ca5ecc7
HoneyPony <honeyponygames@gmail.com>
2026-03-24 07:32:34 -0400

    tests: Add async::if_ tests to build_tests.rs (they do not pass)

--- poniescript/build/build_tests.rs
+++ poniescript/build/build_tests.rs
@@ -54,6 +54,10 @@ pub fn generate(tests_file: &mut File) {
 		("async/", "unbelievably_basic_actual_defer"),
 		("async/", "actual_defer_crazy"),
 		("async/", "unbelievably_basic_binary"),
+		("async/", "if_basic"),
+		("async/", "if_explicit_return"),
+		("async/", "if_explicit_return_2"),
+		("async/", "if_half_diverge"),
 
 		("assign/", "addition_tuple"),
 		("assign/", "arith_ops_float_promote"),
